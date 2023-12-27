218df2d6513d607ee401bb3da5b106d4ae07a521
Moradiya Meet <meetmoradiya@Moradiyas-MacBook-Air.local>
2023-12-27 14:41:19 +0530

    added google meta tag

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -14,6 +14,11 @@ export default function Document() {
         <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="google-adsense-account" content="ca-pub-1428580342799313" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1428580342799313"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
